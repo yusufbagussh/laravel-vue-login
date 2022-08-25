20bb99677109f12840db009b4692531d0ae433a4
rzkalfian <rzkalfian100502@gmail.com>
2022-08-25 13:04:47 +0700

    Aku push ya beb

---
+++ resources/js/views/Registrasi.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+        <div class="wrapper">
+            <div class="text-center mt-4 name">
+                Sign up
+            </div>
+            <form class="p-4 mt-4">
+                <div class="form-field d-flex align-items-center">
+                    <input type="text" name="name" id="name" placeholder="Username">
+                </div>
+                <div class="form-field d-flex align-items-center">
+                    <input type="text" name="email" id="email" placeholder="Email">
+                </div>
+                <div class="form-field d-flex align-items-center">
+                    <input type="password" name="password" id="pwd" placeholder="Password">
+                </div>
+                <div class="form-field d-flex align-items-center">
+                    <input type="password" name="passwordConfirm" id="pwdConf" placeholder="Password Confirmation">
+                </div>
+                <router-link class="btn mt-3" to="/">Submit</router-link>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default{
+    data(){
+        return
+    }
+}
+</script>
+<style>
+.wrapper {
+    max-width: 450px;
+    min-height: 500px;
+    margin: 30px auto;
+    padding: 40px 30px 30px 30px;
+    background-color: #ecf0f3;
+    border-radius: 15px;
+    box-shadow: 13px 13px 20px #cbced1, -13px -13px 20px #fff
+}
+.logo {
+    width: 80px;
+    margin: auto;
+}
+
+.logo img {
+    width: 100%;
+    height: 80px;
+    object-fit: cover;
+    border-radius: 50%;
+    box-shadow: 0px 0px 3px #5f5f5f,
+        0px 0px 0px 5px #ecf0f3,
+        8px 8px 15px #a7aaa7,
+        -8px -8px 15px #fff;
+}
+
+.wrapper .name {
+    font-weight: 7                      00;
+    font-size: 20pt;
+    letter-spacing: 5px;
+    padding-left: 10px;
+    color: #555;
+}
+
+.wrapper .form-field input {
+    width: 100%;
+    display: block;
+    border: none;
+    outline: none;
+    background: none;
+    font-size: 1.2rem;
+    color: #666;
+    padding: 10px 15px 10px 10px;
+    /* border: 1px solid red; */
+}
+
+.wrapper .form-field {
+    padding-left: 10px;
+    margin-bottom: 20px;
+    border-radius: 20px;
+    box-shadow: inset 8px 8px 8px #cbced1, inset -8px -8px 8px #fff;
+}
+
+.wrapper .form-field .fas {
+    color: #555;
+}
+
+.wrapper .btn {
+    box-shadow: none;
+    font-size: 15pt;
+    width: 100%;
+    height: 50px;
+    background-color: #03A9F4;
+    color: #fff;
+    border-radius: 25px;
+    box-shadow: 3px 3px 3px #b1b1b1,
+        -3px -3px 3px #fff;
+    letter-spacing: 1.7px;
+}
+
+.wrapper .btn:hover {
+    background-color: #039BE5;
+}
+
+.wrapper a {
+    text-decoration: none;
+    font-size: 0.8rem;
+    color: #03A9F4;
+}
+
+.wrapper a:hover {
+    color: #039BE5;
+}
+
+</style>
+
