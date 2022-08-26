449ffd4375b69b10260b1829c6244f93db7624b0
yusufbagussh <yusuf.herlambang27@gmail.com>
2022-08-26 15:52:28 +0700

    Membuat Migrasi

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Yusuf Bagus',
+            'email' => 'yusufbagus@gmail.com',
+            'password' => bcrypt('yusuf57552'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
